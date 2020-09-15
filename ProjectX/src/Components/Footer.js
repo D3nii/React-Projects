07764c695222424f477b1696682e384d44608161
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-09-16 02:08:30 +0500

    Update Footer.js

--- ProjectX/src/Components/Footer.js
+++ ProjectX/src/Components/Footer.js
@@ -9,31 +9,30 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <>
-        <table>
-          <tr>
-            <th>Contact</th>
-            <th>Team</th>
-            <th>Subscribe</th>
-          </tr>
-          <tr>
-            <td>num 1</td>
-            <td>join</td>
-          </tr>
-          <tr>
-            <td>num 2</td>
-            <td>join</td>
-            <td>
-              <input type="text" />
-              <button>Subscribe</button>
-            </td>
-          </tr>
-          <tr>
-            <td>num 3</td>
-            <td>join</td>
-          </tr>
-        </table>
-      </>
+      <div id="App">
+        <br />
+        <div id="jake">
+          <div id="jak">
+            <h1>Masti keriyan?</h1>
+            <p>ahsuhdfgjhgfdgshjahsgdfgshajhs</p>
+          </div>
+
+          <div id="jake2">
+            <h2>Community</h2>
+            <p>Facebook</p>
+            <p>Instagram</p>
+          </div>
+          <div id="jake1">
+            <h2>Company</h2>
+            <p>About Us</p>
+            <p>Contact Us</p>
+          </div>
+          <div id="jake1">
+            <h2>Subscribe</h2>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
     );
   }
 }
