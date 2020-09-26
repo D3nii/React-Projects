0ccce6435099d1b932b95cdd2cb3df0cd04449fd
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-09-26 20:44:38 +0500

    Create App.js

---
+++ prox/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+import NavBar from "./Components/NavBar";
+
+import Page1 from "./Components/Page1";
+
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <Page1 />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
