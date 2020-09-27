467237bfd2b6f763a43f714b645cb77d163e58ab
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-09-27 23:35:30 +0500

    Update Footer.js

--- prox/src/Components/Footer.js
+++ prox/src/Components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { linkedin } from "react-icons/fa";
+
 import "../Style/Footer.css";
 
 class Footer extends React.Component {
@@ -12,24 +14,54 @@ class Footer extends React.Component {
       <div id="App">
         <br />
         <div id="col">
-          <div id="jak">
+          <div id="footerMain">
             <h1>Masti keriyan?</h1>
-            <p>Masti keriyan</p>
+            <p>
+              Masti keriyan is an image annotation company for computer vision
+              projects.{" "}
+            </p>
+            <a href={"mailto:" + "EMAIL"}>email</a>
+            <p>+92 300 6405343</p>
+            <span>Get in touch</span>
+            <a href="/">
+              <linkedin />
+              <i class="fab fa-linkedin"></i>
+            </a>
           </div>
 
           <div id="col1">
-            <h2>Community</h2>
-            <p>Facebook</p>
-            <p>Instagram</p>
+            <h2>Solutions</h2>
+            <a href="/">Lines</a>
+            <a href="/">Bounding Boxes</a>
+            <a href="/">Points</a>
+            <a href="/">Polygons</a>
+            <a href="/">Classification</a>
+            <a href="/">Tracking</a>
           </div>
+
           <div id="col1">
-            <h2>Company</h2>
-            <p>About Us</p>
-            <p>Contact Us</p>
+            <h2>Core Values</h2>
+            <a href="/">Quality</a>
+            <a href="/">Security & Privacy</a>
+            <a href="/">Timely Deliveries</a>
+            <a href="/">Lowest Prices</a>
+            <a href="/">Speed</a>
           </div>
+
           <div id="col1">
-            <h2>Subscribe</h2>
-            <input type="text" />
+            <h2>Know Us more</h2>
+            <a href="/">About Us</a>
+          </div>
+
+          <div id="col1">
+            <h2>Join Us</h2>
+            <a href="/">Careers</a>
+          </div>
+
+          <div id="col1">
+            <h2>Legals</h2>
+            <a href="/">Terms of Services</a>
+            <a href="/">Privacy Policy</a>
           </div>
         </div>
       </div>
