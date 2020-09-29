d01ed1cd7cc5233e6783c14d6746d42c6fb98d1d
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2020-09-30 01:03:43 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./App.css";
+
+import NavBar from "./Components/NavBar";
+
+import Page1 from "./Components/Page1";
+import Page2 from "./Components/Page2";
+import Page3 from "./Components/Page3";
+import Slide from "./Components/slideshow";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <Page1 />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Page2 />
+
+      <Page3 />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Slide />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
