c01e54f428c14d5332101777dacc022e5e3b82d1
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-10-02 02:13:49 +0500

    Update App.js

--- prox/src/App.js
+++ prox/src/App.js
@@ -6,6 +6,8 @@ import NavBar from "./Components/NavBar";
 import Page1 from "./Components/Page1";
 import Page2 from "./Components/Page2";
 import Page3 from "./Components/Page3";
+
+import Testionials from "./Components/Testimonials";
 import SlideShow from "./Components/SlideShow";
 import Footer from "./Components/Footer";
 
@@ -57,6 +59,46 @@ function App() {
       <br />
 
       <SlideShow />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h3>What our Clients say about us.</h3>
+      <Testionials
+        message="Masti keriyan? Oh baaz aaja, baaz. Tu badi hraam maut merna ae, tune
+          kuttiyan ne khana ae. Lehaza, chal mera puttar, chutti ker."
+        imageUrl="https://i.ytimg.com/vi/Z4ph23FCcLQ/hqdefault.jpg"
+        name="Abbu tumhara"
+      />
+
+      <Testionials
+        message="Masti keriyan? Oh baaz aaja, baaz. Tu badi hraam maut merna ae, tune
+          kuttiyan ne khana ae. Lehaza, chal mera puttar, chutti ker."
+        imageUrl="https://i.ytimg.com/vi/Z4ph23FCcLQ/hqdefault.jpg"
+        name="Abbu tumhara"
+      />
+
+      <Testionials
+        message="Masti keriyan? Oh baaz aaja, baaz. Tu badi hraam maut merna ae, tune
+          kuttiyan ne khana ae. Lehaza, chal mera puttar, chutti ker."
+        imageUrl="https://i.ytimg.com/vi/Z4ph23FCcLQ/hqdefault.jpg"
+        name="Abbu tumhara"
+      />
       <br />
       <br />
       <br />
