8d492de91ced1e8bf99b4e0fb83a9dd458ff0976
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-10-11 19:45:59 +0500

    Update Page2.js

--- prox/src/Components/Page2.js
+++ prox/src/Components/Page2.js
@@ -2,17 +2,45 @@ import React from "react";
 
 import "../Style/Page2.css";
 
+import { BsBoundingBox } from "react-icons/bs";
+import { AiOutlineFall } from "react-icons/ai";
+import { IoIosContract } from "react-icons/io";
+import { VscSymbolClass } from "react-icons/vsc";
+import { BsLifePreserver } from "react-icons/bs";
+import { GiThreePointedShuriken } from "react-icons/gi";
+import { BiShapePolygon } from "react-icons/bi";
+
+let x = 0;
 var l = "back1";
 
-const lol = (we) => {
+const moveSliderPointerH = (hor) => {
   let x = document.getElementById(l);
-  x.style.width = we + "px";
-  x = document.getElementById("slider2");
-  x.style.marginLeft = we + "px";
-};
 
-const llol = (e) => {
-  lol(e.clientX - 79);
+  x.style.width = hor + "px";
+  
+  x = document.getElementById("slider2");
+
+  if (hor < 500)
+    x.style.marginLeft = hor + "px";
+}; //Controlling the horizontal movement
+
+const moveSliderPointerV = (vert) => {
+  // let x = document.getElementById(l);
+
+  // x.style.width = vert + "px";
+  
+  x = document.getElementById("slider2");
+
+  if (vert < 500)
+    x.style.marginTop = vert + "px";
+}; //Controlling the vertical movement
+
+const moveSliderPointerHelper = (e) => {
+  x = 1;
+
+  moveSliderPointerH(e.clientX - 79);
+  moveSliderPointerV(e.clientY - 350);
+
 };
 
 const first = () => {
@@ -34,6 +62,7 @@ const third = () => {
   document.getElementById("img1").style.display = "none";
   document.getElementById("img2").style.display = "none";
   document.getElementById("img3").style.display = "block";
+
 };
 
 class Page2 extends React.Component {
@@ -44,92 +73,103 @@ class Page2 extends React.Component {
   render() {
     return (
       <>
-        <h1 id="main2" style={{ color: "darkcyan", fontSize: "50px" }}>
+        <h1 id="main2" >
           Services We Provide
         </h1>
         <section id="page2">
           <span id="slider">
             <div className="images">
               <div className="slidecontainer">
+                
                 <div id="img1">
                   <div id="back1"></div>
                   <div id="front1"></div>
                 </div>
+                
                 <div id="img2">
                   <div id="back2"></div>
                   <div id="front2"></div>
                 </div>
+                
                 <div id="img3">
                   <div id="back3"></div>
                   <div id="front3"></div>
                 </div>
 
-                <div id="lol" className="slider" onMouseMove={llol}>
-                  <div id="slider2">LOL</div>
+                <div id="lol" className="slider" onMouseMove={moveSliderPointerHelper}>
+                <div id="slider2"><BsLifePreserver/>
                 </div>
+                  </div>
               </div>
             </div>
           </span>
-
-          <span id="services">
-            <a className="spe" onClick={first}>
-              <div className="sliderOption">
-                <span className="littleHeading">Tracking</span> <br />
-                <span className="littleBody">
-                  For the problems as Surveillance, Monitoring and Enforcement,
-                  detecting and tracking objects in videos.
-                </span>
-              </div>
-            </a>
-
-            <a className="spe" onClick={second}>
-              <div className="sliderOption">
-                <span className="littleHeading">Classification</span> <br />
-                <span className="littleBody">
-                  For classification of images to assign them different classes.
-                </span>
-              </div>
-            </a>
-
-            <a className="spe" onClick={third}>
-              <div className="sliderOption">
-                <span className="littleHeading">Bounding Boxes</span>
-                <br />
-                <span className="littleBody">
-                  Bounding Boxes are quick and cheap to make, especially with
-                  experience. For object detection, localization using
-                  2-dimensional or 3-dimensional bounding boxes.
-                </span>
-              </div>
-            </a>
-            <a className="spe" onClick={second}>
-              <div className="sliderOption">
-                <span className="littleHeading">Polygons</span> <br />
-                <span className="littleBody">
-                  For applications like semantic and instance segmentation.{" "}
-                </span>
-              </div>
-            </a>
-            <a className="spe" onClick={second}>
-              <div className="sliderOption">
-                <span className="littleHeading">Points</span> <br />
-                <span className="littleBody">
-                  For key point estimation in problems in Pose Estimation or
-                  Facial key points Estimation.{" "}
-                </span>
-              </div>
-            </a>
-
-            <a className="spe" onClick={second}>
-              <div className="sliderOption">
-                <span className="littleHeading">Lines</span> <br />
-                <span className="littleBody">
-                  For Main roads / Lanes for the self-driving car technology.
-                </span>
-              </div>
-            </a>
-          </span>
+       
+          <div id="scroll">
+            <span id="services">
+                <div className="sliderOption">
+                  <span className="littleHeading"><IoIosContract id="icons"/> Tracking</span> <br />
+                  <a className="spe" id="section1">
+                  <span className="littleBody"onClick={first}>
+                    For the problems as Surveillance, Monitoring and Enforcement,
+                  <section>detecting and tracking objects in videos.</section> 
+                  </span>
+                  </a>
+                </div>
+              
+                <div className="sliderOption">
+                  <span className="littleHeading"><VscSymbolClass id="icons"/>Classification</span> <br />
+                  <a className="spe" >
+                  <span className="littleBody"onClick={second}>
+                    For classification of images to assign them different classes.
+                  </span>
+                  </a>
+                </div>
+              
+                <div className="sliderOption">
+                  <span className="littleHeading"><BsBoundingBox id="icons"/>Bounding Boxes</span>
+                  <br />
+                  <a className="spe" onClick={third}>
+                  <span className="littleBody">
+                    Bounding Boxes are quick and cheap to make, especially with
+                    experience.
+                    <section> For object detection, localization using
+                    2-dimensional or 3-dimensional</section> <section>bounding boxes.
+                    </section>
+                  </span>
+                  </a>
+                </div>
+          
+                <div className="sliderOption">
+                  <span className="littleHeading"><BiShapePolygon id="icons"/>Polygons</span> <br />
+                  <a className="spe" onClick={third}>
+                  <span className="littleBody">
+                    For applications like semantic and instance segmentation.{" "}
+                  </span>
+                  </a>
+                </div>
+            
+                <div className="sliderOption" >
+                  <span className="littleHeading"><GiThreePointedShuriken id="icons"/>Points</span> <br />
+                  <a className="spe" onClick={second}>
+                  <span className="littleBody">
+                    For key point estimation in problems in Pose Estimation or
+                    Facial key points Estimation.{" "}
+                  </span>
+                  </a>
+                </div>
+            
+                <div className="sliderOption">
+                  <span className="littleHeading"><AiOutlineFall id="icons"/>Lines</span> <br />
+                  <a className="spe" onClick={first} id="section2">
+                  <span className="littleBody">
+                    For Main roads / Lanes for the self-driving car technology.
+                  </span>
+                  </a>
+                </div>
+            </span>
+          </div>
         </section>
+     
       </>
     );
   }
