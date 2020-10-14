8ab204b82badff1c7847b2e4f3dcc71ae28a3cba
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-10-14 21:48:58 +0500

    Update Testimonials.js

--- prox/src/Components/Testimonials.js
+++ prox/src/Components/Testimonials.js
@@ -10,14 +10,11 @@ class Testimonial extends React.Component {
   render() {
     return (
       <div id="testimonial">
-        <div id="invertedComma">"</div>
         <div id="TestimonialWriting">{this.props.message}</div>
-        <div id="baaar">
-          {" "}
-          <hr />{" "}
-        </div>
-        <img id="testimonialPicture" src={this.props.imageUrl} />
-        <div id="testimonialName">{this.props.name}</div>
+        <cite id="testimonialName">
+          <span>{this.props.name}</span>
+          <span>{this.props.desig}</span>
+        </cite>
       </div>
     );
   }
