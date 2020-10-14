5bcd7941f0c62285c7120f6ad5715f0a87e08a63
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-10-14 21:49:03 +0500

    Update Page2.js

--- prox/src/Components/Page2.js
+++ prox/src/Components/Page2.js
@@ -101,85 +101,75 @@ class Page2 extends React.Component {
 
         <div id="scroll">
           <span id="services">
-            <div className="sliderOption">
+            <div className="sliderOption" onClick={first}>
               <span className="littleHeading">
                 <IoIosContract id="icons" /> Tracking
               </span>{" "}
               <br />
-              <a className="spe" id="section1">
-                <span className="littleBody" onClick={first}>
-                  For the problems as Surveillance, Monitoring and Enforcement,
-                  detecting and tracking objects in videos.
-                </span>
-              </a>
+              <span className="littleBody">
+                For the problems as Surveillance, Monitoring and Enforcement,
+                detecting and tracking objects in videos.
+              </span>
             </div>
 
-            <div className="sliderOption">
+            <div className="sliderOption" onClick={second}>
               <span className="littleHeading">
                 <VscSymbolClass id="icons" />
                 Classification
               </span>{" "}
               <br />
-              <a className="spe">
-                <span className="littleBody" onClick={second}>
-                  For classification of images to assign them different classes.
-                </span>
-              </a>
-            </div>
-
-            <div className="sliderOption">
-              <span className="littleHeading">
-                <BsBoundingBox id="icons" />
-                Bounding Boxes
+              <span className="littleBody">
+                For classification of images to assign them different classes.
               </span>
-              <br />
-              <a className="spe" onClick={third}>
-                <span className="littleBody">
-                  Bounding Boxes are quick and cheap to make, especially with
-                  experience. For object detection, localization using
-                  2-dimensional or 3-dimensional bounding boxes.
-                </span>
-              </a>
             </div>
 
-            <div className="sliderOption">
+            <div className="sliderOption" onClick={third}>
+              <a className="spe">
+                <span className="littleHeading">
+                  <BsBoundingBox id="icons" />
+                  Bounding Boxes
+                </span>
+              </a>
+              <br />
+              <span className="littleBody">
+                Bounding Boxes are quick and cheap to make, especially with
+                experience. For object detection, localization using
+                2-dimensional or 3-dimensional bounding boxes.
+              </span>
+            </div>
+
+            <div className="sliderOption" onClick={third}>
               <span className="littleHeading">
                 <BiShapePolygon id="icons" />
                 Polygons
               </span>{" "}
               <br />
-              <a className="spe" onClick={third}>
-                <span className="littleBody">
-                  For applications like semantic and instance segmentation.{" "}
-                </span>
-              </a>
+              <span className="littleBody">
+                For applications like semantic and instance segmentation.{" "}
+              </span>
             </div>
 
-            <div className="sliderOption">
+            <div className="sliderOption" onClick={second}>
               <span className="littleHeading">
                 <GiThreePointedShuriken id="icons" />
                 Points
               </span>{" "}
               <br />
-              <a className="spe" onClick={second}>
-                <span className="littleBody">
-                  For key point estimation in problems in Pose Estimation or
-                  Facial key points Estimation.{" "}
-                </span>
-              </a>
+              <span className="littleBody">
+                For key point estimation in problems in Pose Estimation or
+                Facial key points Estimation.{" "}
+              </span>
             </div>
 
-            <div className="sliderOption">
+            <div className="sliderOption" onClick={first}>
               <span className="littleHeading">
                 <AiOutlineFall id="icons" />
                 Lines
               </span>{" "}
               <br />
-              <a className="spe" onClick={first} id="section2">
-                <span className="littleBody">
-                  For Main roads / Lanes for the self-driving car technology.
-                </span>
-              </a>
+              <span className="littleBody">
+                For Main roads / Lanes for the self-driving car technology.
+              </span>
             </div>
           </span>
         </div>
