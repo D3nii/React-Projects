7b4b117ddc130c46ad2042dd915e9bbacfd50f76
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2021-01-25 00:43:34 +0500

    Create precache-manifest.a4d7e243aa56721f39f6fbf28374fe7a.js

---
+++ prox/build/precache-manifest.a4d7e243aa56721f39f6fbf28374fe7a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9267f2a8ac24fccac89492ae7696a88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7e50a4a34fadc40765",
+    "url": "/static/css/main.c6035da6.chunk.css"
+  },
+  {
+    "revision": "e24a839856780180f368",
+    "url": "/static/js/2.80498287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80498287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7e50a4a34fadc40765",
+    "url": "/static/js/main.c7130e19.chunk.js"
+  },
+  {
+    "revision": "3a6a248beeb5debb4733",
+    "url": "/static/js/runtime-main.c8223320.js"
+  },
+  {
+    "revision": "794a3f9fa88e8ffdb3a7f418e23e21d0",
+    "url": "/static/media/bounding_boxes.794a3f9f.jpg"
+  },
+  {
+    "revision": "7b40a0c438d7695a0462046d6566cfe3",
+    "url": "/static/media/down1.7b40a0c4.jpeg"
+  },
+  {
+    "revision": "2fc8a2607125c68cc5c171d445d873a1",
+    "url": "/static/media/down_arrow.2fc8a260.gif"
+  },
+  {
+    "revision": "83e9a8cb4164d03d08e5a401dd3d12f2",
+    "url": "/static/media/lines.83e9a8cb.jpg"
+  },
+  {
+    "revision": "19f0d741b35d3126121f6157dd463803",
+    "url": "/static/media/polygon_normal.19f0d741.png"
+  },
+  {
+    "revision": "dae1169911a088a2d1078d2098438bad",
+    "url": "/static/media/segment.dae11699.jpg"
+  },
+  {
+    "revision": "b037acc410649578bc5c2a893de4167f",
+    "url": "/static/media/try2.b037acc4.jpg"
+  },
+  {
+    "revision": "b595f5e65c48068012ef10eef83a786a",
+    "url": "/static/media/up1.b595f5e6.jpeg"
+  }
+]);
