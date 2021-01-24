cab11de48664fe6d63fece071da5209ef8156442
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2021-01-25 00:44:38 +0500

    Update Page1.js

--- prox/src/Components/Page1.js
+++ prox/src/Components/Page1.js
@@ -10,14 +10,16 @@ class Page1 extends React.Component {
   render() {
     return (
       <div id="page1">
-        <div id="main">
-          <span style={{ color: "rgb(78, 83, 119)" }}>Image Annotation</span>{" "}
-          <br /> for <br /> Computer Vision
-        </div>
-        <div id="leftover">
-          We provide high quality food for your machine learning models <br />{" "}
-          so that they grow healthy and fat.{" "}
-        </div>
+        <div id="page1content">
+          <div id="main">
+            <span style={{ color: "rgb(78, 83, 119)" }}>Image Annotation</span>{" "}
+            <br /> for <br /> Computer Vision
+          </div>
+          <div id="leftover">
+            We provide high quality food for your machine learning models <br />{" "}
+            so that they grow healthy and fat.{" "}
+          </div>
+        </div>  
       </div>
     );
   }
